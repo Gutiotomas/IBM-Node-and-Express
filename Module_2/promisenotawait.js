ef60996425cd566fdd8e857ed24460b57207beec
Tomás Gutiérrez Orrego <111087086+Gutiotomas@users.noreply.github.com>
2024-10-17 17:21:33 -0500

    Add promises that not await one or another and run independently

---
+++ Module_2/promisenotawait.js
@@ -0,0 +1,20 @@
+//Creating a promise method. The promise will get resolved when timer times out after 6 seconds.
+let myPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Promise resolved");
+  }, 6000);
+});
+
+let myPromise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Promise 2 resolved");
+  }, 3000);
+});
+//Call the promise and wait for it to be resolved and then print a message.
+myPromise.then((successMessage) => {
+  console.log("From Callback " + successMessage);
+});
+//Call the promise and wait for it to be resolved and then print a message.
+myPromise2.then((successMessage) => {
+  console.log("From Callback " + successMessage);
+});
